6facbb2c412e072a4e3c3a697c7b846592ad5894
hundeva <hundeva@gmail.com>
2019-10-12 07:33:14 +0000

    Launcher3: Add restart action for toggling settings
    Squashed:
    
        From: elpaablo <donopwhile1@gmail.com>
        Date: Wed, 17 Mar 2021 11:11:29 +0000
        Subject: [PATCH 094/300] Launcher3: Move restart check to home page
    
        It's just a flag check. And avoids having to implement a listener for the home key (and still miss home gesture).
        Also, restarting the launcher when navigating to previous settings page doesn't seem right.
        This way, the launcher is restarted once, if needed, when user goes back to home page, which is where the changes are visible, anyway.
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    @neobuddy89: Updated for Android 15 and Android 16.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I161eb494b0638929418359ef99320ab086abb2a2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -1328,6 +1328,8 @@ public class Launcher extends StatefulActivity<LauncherState>
 
         DragView.removeAllViews(this);
         TraceHelper.INSTANCE.endSection();
+
+        LauncherAppState.INSTANCE.executeIfCreated(app -> app.checkIfRestartNeeded());
     }
 
     @Override
--- src/com/android/launcher3/quickspace/QuickspaceController.java
+++ src/com/android/launcher3/quickspace/QuickspaceController.java
@@ -29,7 +29,7 @@ import android.media.session.PlaybackState;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.android.internal.util.crdroid.OmniJawsClient;
+import com.android.internal.util.witaqua.OmniJawsClient;
 
 import com.android.launcher3.LauncherPrefs;
 import com.android.launcher3.R;
