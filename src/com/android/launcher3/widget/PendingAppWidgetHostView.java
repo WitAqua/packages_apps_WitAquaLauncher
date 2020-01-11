d6d2479cca822cf6bcfe2294d8adc61589e67c62
Amir Zaidi <azaidi@live.nl>
2020-01-11 13:55:58 +0100

    Launcher3: Add Icon Pack Support from Shade Launcher [SQUASHED]
    DennySPb: port to A11, A12-L, A13
    
    @neobuddy89:
    Updated for A13 QPR2, A14 QPR2, A14 QPR3, A15, A15 QPR2, A16
    
    Squashed:
    
        From: Nauval Rizky <enuma.alrizky@gmail.com>
        Date: Sun, 7 May 2023 22:32:18 +0700
        Subject: Launcher3: Use overridden IconProvider instance consistently
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: DennySPb <dennyspb@gmail.com>
        Date: Tue, 15 Nov 2022 09:29:00 +0300
        Subject: Launcher3: InfoBottomSheet: Fix NPE with AppTransitionManager
    
        Signed-off-by: DennySPb <dennyspb@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Sun, 13 Nov 2022 03:21:12 +0530
        Subject: Launcher3: Make icon pack support themed icons
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: Id81da397d5ef33577d6f53f349080c49a61591ab
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/widget/PendingAppWidgetHostView.java
+++ src/com/android/launcher3/widget/PendingAppWidgetHostView.java
@@ -53,12 +53,14 @@ import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.Launcher;
 import com.android.launcher3.LauncherAppState;
 import com.android.launcher3.R;
 import com.android.launcher3.icons.FastBitmapDrawable;
+import com.android.launcher3.graphics.DrawableFactory;
 import com.android.launcher3.icons.IconCache.ItemInfoUpdateReceiver;
 import com.android.launcher3.model.data.ItemInfoWithIcon;
 import com.android.launcher3.model.data.LauncherAppWidgetInfo;
@@ -293,6 +295,7 @@ public class PendingAppWidgetHostView extends LauncherAppWidgetHostView
 
     @Override
     public void reapplyItemInfo(ItemInfoWithIcon info) {
+        DrawableFactory drawableFactory = DrawableFactory.INSTANCE.get(getContext());
         if (mCenterDrawable != null) {
             mCenterDrawable.setCallback(null);
             mCenterDrawable = null;
@@ -304,18 +307,18 @@ public class PendingAppWidgetHostView extends LauncherAppWidgetHostView
         //   2) Preload icon in the center
         //   3) App icon in the center with a setup icon on the top left corner.
         if (mDisabledForSafeMode) {
-            FastBitmapDrawable disabledIcon = info.newIcon(getContext());
+            FastBitmapDrawable disabledIcon = drawableFactory.newIcon(getContext(), info);
             disabledIcon.setIsDisabled(true);
             mCenterDrawable = disabledIcon;
             mSettingIconDrawable = null;
         } else if (isReadyForClickSetup()) {
-            mCenterDrawable = info.newIcon(getContext());
-            mSettingIconDrawable = getResources().getDrawable(R.drawable.ic_setting).mutate();
+            mCenterDrawable = drawableFactory.newIcon(getContext(), info);
+            mSettingIconDrawable = ContextCompat.getDrawable(getContext(), R.drawable.ic_setting).mutate();
             updateSettingColor(info.bitmap.color);
 
             mDragFlags |= FLAG_DRAW_SETTINGS | FLAG_DRAW_LABEL;
         } else {
-            mCenterDrawable = newPendingIcon(getContext(), info);
+            mCenterDrawable = drawableFactory.newPendingIcon(getContext(), info);
             mSettingIconDrawable = null;
             applyState();
         }
