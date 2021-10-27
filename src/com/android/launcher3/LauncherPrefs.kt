b5be67b799e2d9d507c7b4a358037feb8adb6934
Patryk Michalik <contact@patrykmichalik.com>
2021-10-27 17:48:37 +0200

    Launcher3: Add ability to change corner radius of Dock search bar
    @neobuddy89: Rewrite and adapted for launcher3.
    
    Change-Id: I82c699fa7e9d17335b0d95eca95a21b1af5c8a6f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -247,6 +247,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val DRAWER_OPEN_KEYBOARD = backedUpItem("pref_drawer_open_keyboard", false)
         @JvmField val DOCK_SEARCH = backedUpItem("pref_dock_search", true)
         @JvmField val DOCK_THEME = backedUpItem("pref_dock_theme", false)
+        @JvmField val SEARCH_RADIUS_SIZE = backedUpItem("pref_search_radius_size", 100)
         @JvmField val SHOW_DESKTOP_LABELS = backedUpItem("pref_desktop_show_labels", true)
         @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
         @JvmField val SHOW_QUICKSPACE = backedUpItem("pref_quickspace", true)
