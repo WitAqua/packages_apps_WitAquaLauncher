714a26d4f8d36037e89722524b0f9c8bd1bf7d08
Nauval Rizky <enuma.alrizky@gmail.com>
2022-01-04 15:33:23 +0700

    Launcher3: Bind SeraphixDataProvider to the quickspace
    @neobuddy89:
    * Add weather provider preference to switch between OmniJaws and Google SmartSpace.
    * Bind/unbind directly in QuickspaceController.
    * Mark as weather available if either of icon or weather text is available as icon update could be delayed.
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I54e56eb811182b52fa136adc0f507c3a92bec302
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -114,6 +114,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
                 LauncherPrefs.SHOW_HOTSEAT_BG.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.HOTSEAT_OPACITY.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.DOCK_MUSIC_SEARCH.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.SHOW_STATUS_BAR.getSharedPrefKey().equals(key) ||
                 LauncherPrefs.SHOW_STATUS_BAR.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
