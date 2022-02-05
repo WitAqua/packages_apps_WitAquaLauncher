0a5544ea558b7d5bb024b195bbf5ed8c12e040a1
Pranav Vashi <neobuddy89@gmail.com>
2022-02-06 02:39:35 +0530

    Launcher3: Restart launcher on google app change
    * Refreshes google elements in home screen.
    
    Change-Id: I9bb7f705ac2069cf3e74d975fb2e86e9ca79a562
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -161,6 +161,8 @@ public final class Utilities {
 
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
 
+    public static final String GSA_PACKAGE = "com.google.android.googlequicksearchbox";
+
     /**
      * Returns true if theme is dark.
      */
