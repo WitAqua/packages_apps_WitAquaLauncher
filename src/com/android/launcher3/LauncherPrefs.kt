e8a8cc029980e67d53a4acbf82df4c80fb9d6adc
StarkDroid <gamerprince.exp@gmail.com>
2022-11-29 15:48:29 +0800

    Launcher3: Add toggle to show/hide app drawer search bar
    Change-Id: I0c03fde326700fe826f249f7c813034d27900b9f
    Signed-off-by: StarkDroid <gamerprince.exp@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -247,6 +247,7 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val DRAWER_OPEN_KEYBOARD = backedUpItem("pref_drawer_open_keyboard", false)
         @JvmField val DOCK_SEARCH = backedUpItem("pref_dock_search", true)
         @JvmField val DOCK_THEME = backedUpItem("pref_dock_theme", false)
+        @JvmField val DRAWER_SEARCH = backedUpItem("pref_drawer_search", true)
         @JvmField val HOTSEAT_OPACITY = backedUpItem("pref_hotseat_opacity", 40)
         @JvmField val SEARCH_RADIUS_SIZE = backedUpItem("pref_search_radius_size", 100)
         @JvmField val SHOW_HOTSEAT_BG = backedUpItem("pref_show_hotseat_bg", false)
